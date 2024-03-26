d1a45f0df2301bc4c5d4061e49c390f258ca6058
Hyuck <lhg1304@nexmore.co.kr>
2024-03-26 10:47:25 +0900

    [Sample Controller] message API 추가

--- second-service/src/main/java/com/example/secondservice/SecondController.java
+++ second-service/src/main/java/com/example/secondservice/SecondController.java
@@ -1,9 +1,12 @@
 package com.example.secondservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/second-service")
 public class SecondController {
@@ -12,4 +15,11 @@ public class SecondController {
 	public String welcome() {
 		return "Welome to the Second service.";
 	}
+
+	@GetMapping("message")
+	public String message(@RequestHeader("second-request") String header) {
+		log.info(header);
+		return "Hello World in Second Service.";
+	}
+
 }
