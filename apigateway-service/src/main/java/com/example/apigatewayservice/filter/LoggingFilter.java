b614883d66db3e2b114973b4c63e16479c8bfd11
Hyuck <lhg1304@nexmore.co.kr>
2024-03-26 11:58:11 +0900

    [API Gateway Service] Logging Filter 적용

--- apigateway-service/src/main/java/com/example/apigatewayservice/filter/LoggingFilter.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/filter/LoggingFilter.java
@@ -3,38 +3,41 @@ package com.example.apigatewayservice.filter;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
+
 @Slf4j
 @Component
-public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Config> {
 
-    public GlobalFilter() {
+    public LoggingFilter() {
         super(Config.class);
     }
 
     @Override
     public GatewayFilter apply(Config config) {
-        return (exchange, chain) -> {
+        GatewayFilter filter = new OrderedGatewayFilter((exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
             ServerHttpResponse response = exchange.getResponse();
 
-            log.info("Global Filter baseMessage: {}", config.getBaseMessage());
-
+            log.info("Logging Filter baseMessage: {}", config.getBaseMessage());
             if (config.isPreLogger()) {
-                log.info("Global Filter Start: request id -> {}", request.getId());
+                log.info("Logging PRE Filter: request id -> {}", request.getId());
             }
-
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
                 if (config.isPostLogger()) {
-                    log.info("Global Filter End: response code -> {}", response.getStatusCode());
+                    log.info("Logging POST Filter: response code -> {}", response.getStatusCode());
                 }
             }));
-        };
+        }, HIGHEST_PRECEDENCE); // 해당 Filter의 우선순위를 Highest로 잡았기 때문에 Filter 중 가장 먼저 실행됨
+
+        return filter;
     }
 
     @Data
